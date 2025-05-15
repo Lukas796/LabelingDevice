60e21bb086a5febbba7c1d5b643d53476fcb40ca
Lukas <strittmattlukas@gmail.com>
2025-05-15 13:15:08 +0200

    Initial commit: Add project files and .gitignore

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * LabelingDevice.c
+ *
+ * Created: 15.05.2025 12:49:39
+ * Authors : lukasstrittmatter, robinschad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
