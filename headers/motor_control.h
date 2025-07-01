7a9b64741d6617b8afd9f03bd1f3b5059fe4f2d4
Lukas Strittmatter <strittmattlukas@gmail.com>
2025-07-01 11:16:20 +0200

    comment motor_control.h

--- headers/motor_control.h
+++ headers/motor_control.h
@@ -11,6 +11,9 @@
 
 #include "config.h" // need to be first
 
+//-----------------------
+//--- FUNKTIONEN -------- 
+
 uint16_t act_Pos_x(void);
 uint16_t act_Pos_y(void);
 uint16_t act_Pos_z(void);
@@ -40,25 +43,25 @@ void move_XZ_diagonal_relative(int32_t rel_steps_x, int32_t rel_steps_z, uint16_
 
 
 //IO-Port-Mapping for the EndSensors for X and Y
-#define X_SWITCH_TOP_PORT 		PORTD
-#define X_SWITCH_TOP_IN_REG		PIND
-#define X_SWITCH_TOP_DDR		DDRD
-#define X_SWITCH_TOP_PIN		PD0
+#define X_SWITCH_TOP_PORT       PORTD      // Ausgangsregister für Pull-Up (z.?B. HIGH setzen)
+#define X_SWITCH_TOP_IN_REG     PIND       // Eingangsregister, um Pin-Zustand (LOW/HIGH) zu lesen
+#define X_SWITCH_TOP_DDR        DDRD       // Data Direction Register – legt fest, ob Pin Ein- oder Ausgang ist
+#define X_SWITCH_TOP_PIN        PD0        // Der konkrete Pin (Pin D0 = digitaler Pin 0)
 
-#define X_SWITCH_BOTTOM_PORT 	PORTD		//Output register
-#define X_SWITCH_BOTTOM_IN_REG 	PIND		//Input register
-#define X_SWITCH_BOTTOM_DDR		DDRD
-#define X_SWITCH_BOTTOM_PIN		PD2
+#define X_SWITCH_BOTTOM_PORT    PORTD      // PORTD: für z.?B. internen Pull-Up aktivieren
+#define X_SWITCH_BOTTOM_IN_REG  PIND       // PIND: Eingang lesen
+#define X_SWITCH_BOTTOM_DDR     DDRD       // DDRD: Richtung (Input/Output) einstellen
+#define X_SWITCH_BOTTOM_PIN     PD2        // Angeschlossen an Pin D2
 
-#define Y_SWITCH_LEFT_PORT 		PORTD
-#define Y_SWITCH_LEFT_IN_REG	PIND
-#define Y_SWITCH_LEFT_DDR		DDRD
-#define Y_SWITCH_LEFT_PIN		PD1
+#define Y_SWITCH_LEFT_PORT      PORTD      // PORTD: für Pull-Up etc.
+#define Y_SWITCH_LEFT_IN_REG    PIND       // Eingang lesen
+#define Y_SWITCH_LEFT_DDR       DDRD       // Richtung festlegen
+#define Y_SWITCH_LEFT_PIN       PD1        // Pin D1
 
-#define Y_SWITCH_RIGHT_PORT 	PORTD
-#define Y_SWITCH_RIGHT_IN_REG	PIND
-#define Y_SWITCH_RIGHT_DDR		DDRD
-#define Y_SWITCH_RIGHT_PIN		PD3
+#define Y_SWITCH_RIGHT_PORT 	PORTD		// PORTD: für Pull-Up etc.
+#define Y_SWITCH_RIGHT_IN_REG	PIND		// Eingangsregister
+#define Y_SWITCH_RIGHT_DDR		DDRD		// Datadirection Register
+#define Y_SWITCH_RIGHT_PIN		PD3			// Anschluss Pin PD3 für Switch Right
 
 #define EXT_INT_CONTROL_REG       EICRA  // External Interrupt Control Register A
 #define EXT_INT_MASK_REG          EIMSK  // External Interrupt Mask Register
@@ -103,41 +106,41 @@ void move_XZ_diagonal_relative(int32_t rel_steps_x, int32_t rel_steps_z, uint16_
 #define EN_Y_PIN    PA0		//Pin PA0 - Digital Pin 22 for STEP Axis Y
 
 // Y-Axis uses for STEP PB6 --> Timer OC1B
-#define TIMER_Y          1
-#define STEP_Y_OC_PIN    PB6
-#define STEP_Y_OC_REG    TCCR1A
-#define STEP_Y_OC_BIT    COM1B0
-#define STEP_Y_OCR       OCR1A
-#define STEP_Y_TCNT      TCNT1
-#define STEP_Y_TIMSK_REG TIMSK1
-#define STEP_Y_OCIE_BIT  OCIE1B
-#define STEP_Y_TCCRB_REG TCCR1B
+#define TIMER_Y          1			// Verwendung von Timer 1 für die Y-Achse
+#define STEP_Y_OC_PIN    PB6		// Der STEP-Ausgang der Y-Achse ist auf Pin PB6 (OC1B)
+#define STEP_Y_OC_REG    TCCR1A		// Steuerregister A von Timer 1 (für Compare Match Einstellungen)
+#define STEP_Y_OC_BIT    COM1B0		// Bit in TCCR1A, das das Verhalten von OC1B (PB6) steuert (Toggle bei Compare Match)
+#define STEP_Y_OCR       OCR1A		// Vergleichswertregister für den Timer 1 ? bestimmt die Frequenz
+#define STEP_Y_TCNT      TCNT1		// Aktueller Zählerstand des Timers 1 
+#define STEP_Y_TIMSK_REG TIMSK1		// Interrupt-Maskenregister für Timer 1 (zur Aktivierung von Compare-Match-Interrupts)
+#define STEP_Y_OCIE_BIT  OCIE1B		// Interrupt Enable Bit für Compare Match B (wird in TIMSK1 gesetzt)
+#define STEP_Y_TCCRB_REG TCCR1B		// Steuerregister B von Timer 1 (wird u.?a. für den Prescaler genutzt)
 
 // Z-Achse (Takt)
-#define STEP_Z_PORT PORTH	//Output-Port for DIR Axis-X
-#define STEP_Z_DDR  DDRH	//Data Direction Register for DIR Axis-X
-#define STEP_Z_PIN  PH4		//Pin PH4 - Digital Pin 7 for STEP Axis X
+#define STEP_Z_PORT PORTH	//Output-Port for STEP Axis-Z
+#define STEP_Z_DDR  DDRH	//Data Direction Register for DIR Axis-Z
+#define STEP_Z_PIN  PH4		//Pin PH4 - Digital Pin 7 for STEP Axis Z
 
 // Z-Achse (Drehrichtung)
-#define DIR_Z_PORT  PORTL
-#define DIR_Z_DDR   DDRL
-#define DIR_Z_PIN   PL2
+#define DIR_Z_PORT  PORTL	//Output-Port for DIR Axis-Z
+#define DIR_Z_DDR   DDRL	//Data Direction Register for DIR_Axis-Z
+#define DIR_Z_PIN   PL2		// Pin PL2 for DIR-AXIS Z
 
 // Z-Achse (Enable)
-#define EN_Z_PORT  PORTL
-#define EN_Z_DDR   DDRL
-#define EN_Z_PIN   PL3
+#define EN_Z_PORT  PORTL	//Output-Port for EN Axis-Z
+#define EN_Z_DDR   DDRL		//Data Direction Register for EN-Z
+#define EN_Z_PIN   PL3		// Pin PL3 for DIR-AXIS Z
 
 //  Z-Axis uses for STEP PH4 --> Timer OC4B
-#define TIMER_Z          4
-#define STEP_Z_OC_PIN    PH4        // OC4B
-#define STEP_Z_OC_REG    TCCR4A
-#define STEP_Z_OC_BIT    COM4B0
-#define STEP_Z_OCR       OCR4A
-#define STEP_Z_TCNT      TCNT4
-#define STEP_Z_TIMSK_REG TIMSK4
-#define STEP_Z_OCIE_BIT  OCIE4B
-#define STEP_Z_TCCRB_REG TCCR4B
+#define TIMER_Z          4             // Verwendung von Timer 4 für die Z-Achse
+#define STEP_Z_OC_PIN    PH4           // STEP-Ausgang der Z-Achse ist Pin H4 (OC4B)
+#define STEP_Z_OC_REG    TCCR4A        // Steuerregister A für Timer 4 (für Compare Match-Konfiguration)
+#define STEP_Z_OC_BIT    COM4B0        // Compare Match Output Mode Bit B – OC4B toggeln bei Match
+#define STEP_Z_OCR       OCR4A         // Output Compare Register A von Timer 4 (legt Frequenz fest)
+#define STEP_Z_TCNT      TCNT4         // Zählerstand des Timer 4 (wird bei Start zurückgesetzt)
+#define STEP_Z_TIMSK_REG TIMSK4        // Interrupt-Maskenregister von Timer 4 (zum Aktivieren von Interrupts)
+#define STEP_Z_OCIE_BIT  OCIE4B        // Interrupt Enable Bit für Compare Match B (für OC4B)
+#define STEP_Z_TCCRB_REG TCCR4B        // Steuerregister B von Timer 4 (enthält Prescaler-Einstellungen)
 
 
 #endif /* MOTOR_CONTROL_H_ */
