36d25b06dc7e91199dbd5ac1cd8c06837dccf0bf
Lukas Strittmatter <strittmattlukas@gmail.com>
2025-07-29 13:06:15 +0200

    Reapply "comitt comments"
    This reverts commit d78e4b3bdf9c68f62911a8941aad648984ab0658.

--- sourceFiles/lcd_control.c
+++ sourceFiles/lcd_control.c
@@ -14,103 +14,105 @@
 #define RS PC2  // Register Select
 #define EC PC3  // Enable
 
-void lcd_char(char t)  // Zeichen senden
-{
+void lcd_char(char t)  // Zeichen senden                                // Sendet ein Zeichen an das LCD
+{                                                                         // Beginn lcd_char
 	// Oberes Halbbyte
-	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4));
-	PORTC |= (t & 0xF0) | (1 << EC) | (1 << RS);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
+	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4));       // Löscht obere 4 Datenleitungen
+	PORTC |= (t & 0xF0) | (1 << EC) | (1 << RS);                         // Setzt obere Bits, EC und RS
+	PORTC &= ~(1 << EC);                                                 // EC zurücksetzen
+	_delay_ms(1);                                                        
 
 	// Unteres Halbbyte
-	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4));
-	PORTC |= (t << 4) | (1 << EC) | (1 << RS);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
-}
+	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4));       // Löscht obere 4 Datenleitungen
+	PORTC |= (t << 4) | (1 << EC) | (1 << RS);                           // Setzt untere Bits, EC und RS
+	PORTC &= ~(1 << EC);                                                 // EC zurücksetzen
+	_delay_ms(1);                                                        
+}                                                                       
 
-void lcd_cmd(uint8_t cmd) // Befehl senden
-{
+void lcd_cmd(uint8_t cmd) // Befehl senden                              // Sendet einen Steuerbefehl ans LCD
+{                                                                        
 	// Oberes Halbbyte
-	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4) | (1 << RS));
-	PORTC |= (cmd & 0xF0) | (1 << EC);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
+	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4) | (1 << RS));  // Löscht obere Bits und RS
+	PORTC |= (cmd & 0xF0) | (1 << EC);                                    // Setzt obere Bits und EC
+	PORTC &= ~(1 << EC);                                                  // EC zurücksetzen
+	_delay_ms(1);                                                         
 
 	// Unteres Halbbyte
-	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4) | (1 << RS));
-	PORTC |= (cmd << 4) | (1 << EC);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
-}
+	PORTC &= ~((1 << PC7) | (1 << PC6) | (1 << PC5) | (1 << PC4) | (1 << RS));  // Löscht obere Bits und RS
+	PORTC |= (cmd << 4) | (1 << EC);                                      // Setzt untere Bits und EC
+	PORTC &= ~(1 << EC);                                                  // EC zurücksetzen
+	_delay_ms(1);                                                         // Kurze Verzögerung
+}                                                                         
 
-void lcd_text(char* text)
-{
-	uint8_t j = 0;
-	while (text[j] != '\0') 
-	{
-		lcd_char(text[j]);  // Einzelne Zeichen senden
-		j++;
-	}
-}
+void lcd_text(char* text)                                                  // Gibt eine Zeichenkette aus
+{                                                                        
+	uint8_t j = 0;                                                        // Initialisiert Index j
+	while (text[j] != '\0')                                               // Schleife bis Nullterminator
+	{                                                                     // Schleifenkörper
+		lcd_char(text[j]);                                                // Sende aktuelles Zeichen
+		j++;                                                              // Index erhöhen
+	}                                                                     
+}                                                                         
 
-void lcd_Pos(uint8_t zeile, uint8_t spalte) // Cursor-Position setzen
-{
-	uint8_t spalte_temp = (spalte - 1);          // Spalte anpassen, da LCDs bei 0 starten
-	uint8_t zeile_temp = (zeile - 1) * 0x40;     // Zeile berechnen (LCD-Zeilen haben feste Speicheradressen)
-	uint8_t pos = (spalte_temp + zeile_temp) | 0x80; // Adresse berechnen und DB7-Bit setzen
-	lcd_cmd(pos);  // Position an das LCD senden
-}
+void lcd_Pos(uint8_t zeile, uint8_t spalte) // Cursor-Position setzen     // Positioniert den Cursor
+{                                                                       
+	uint8_t spalte_temp = (spalte - 1);                                   // Nullbasiertes Spalten-Offset
+	uint8_t zeile_temp = (zeile - 1) * 0x40;                              // Zeilenoffset im DDRAM
+	uint8_t pos = (spalte_temp + zeile_temp) | 0x80;                      // Adresse mit Set-Bit
+	lcd_cmd(pos);                                                         // Sende Positionsbefehl
+}                                                                         
 
-void lcd_num(uint16_t num,char* text)
-{
-	char char1;			// variable for 100s
-	char char2;			// variable for 10s
-	
-	char1 = num/100 + 0x30; // charachter for 100s
-	num = num % 100;		// remainder
-	
-	char2 = num/10 + 0x30;	// character for 10s
-	num = num % 10;			// remainder
-	
-	text[0] = char1;		//
-	text[1] = char2;		//
-	text[2] = num + 0x30;	//
-	text[3] = 0x00;			// end of string
-}
+void lcd_num(uint16_t num,char* text)                                     // Wandelt Zahl in ASCII um
+{                                                                         // Beginn lcd_num
+	char char1;                                                           // Hunderterstelle
+	char char2;                                                           // Zehnerstelle
 
-void lcd_init(void)
-{
-	DDRC = 0xFC;
-	_delay_ms(20);
-	PORTC &= ~((1 << RS) | (1 << PC6) | (1 << PC7));
-	PORTC |= (1 << PC5) | (1 << PC4) | (1 << EC);
-	PORTC &= ~(1 << EC);
+	char1 = num/100 + 0x30;                                               // ASCII Hunderter
+	num = num % 100;                                                      // Rest bestimmen
 
-	_delay_ms(5);
-	PORTC |= (1 << EC);
-	PORTC &= ~(1 << EC);
+	char2 = num/10 + 0x30;                                                // ASCII Zehner
+	num = num % 10;                                                       // Rest bestimmen
 
-	_delay_ms(1);
-	PORTC |= (1 << EC);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
+	text[0] = char1;                                                      // Setze Hunderter
+	text[1] = char2;                                                      // Setze Zehner
+	text[2] = num + 0x30;                                                 // Setze Einer
+	text[3] = 0x00;                                                       // Nullterminator
+}                                                                       
 
-	// Wechsel in den 4-Bit-Modus
-	PORTC &= ~(1 << PC4);
-	PORTC |= (1 << EC);
-	PORTC &= ~(1 << EC);
-	_delay_ms(1);
+void lcd_init(void)                                                       // Initialisiert das LCD
+{                                                                        
+	DDRC = 0xFC;                                                          // PC2..PC7 als Ausgang
+	_delay_ms(20);                                                        // Warte nach Power-On
 
-	lcd_cmd(0x28);
-	lcd_cmd(0x08);
-	lcd_cmd(0x01);
-	_delay_ms(2);
+	PORTC &= ~((1 << RS) | (1 << PC6) | (1 << PC7));                      // Lösche RS und Datenleitungen
+	PORTC |= (1 << PC5) | (1 << PC4) | (1 << EC);                         // Erste Pulse senden
+	PORTC &= ~(1 << EC);                                                  // EC zurücksetzen
 
-	lcd_cmd(0x06);
-	lcd_cmd(0x14);
-	lcd_cmd(0x0F);
-	_delay_ms(2);
-}
+	_delay_ms(5);                                                         
+	PORTC |= (1 << EC);                                                   // Enable setzen
+	PORTC &= ~(1 << EC);                                                  // Enable zurücksetzen
+
+	_delay_ms(1);                                                        
+	PORTC |= (1 << EC);                                                   // Enable setzen
+	PORTC &= ~(1 << EC);                                                  // Enable zurücksetzen
+	_delay_ms(1);                                                         
+
+	// Wechsel in 4-Bit-Modus
+	PORTC &= ~(1 << PC4);                                                 // Halbes Kommando senden
+	PORTC |= (1 << EC);                                                   // Enable setzen
+	PORTC &= ~(1 << EC);                                                  // Enable zurücksetzen
+	_delay_ms(1);                                                        
+
+	lcd_cmd(0x28);                                                        // 4-Bit, 2 Zeilen, 5x8
+	lcd_cmd(0x08);                                                        // Display aus
+	lcd_cmd(0x01);                                                        // Display löschen
+	_delay_ms(2);                                                         
+
+	lcd_cmd(0x06);                                                        // Cursor inkrement
+	lcd_cmd(0x14);                                                        // Cursor verschieben
+	lcd_cmd(0x0F);                                                        // Display an, Cursor blinkt
+	_delay_ms(2);                                                         
+}                                                                         
+
 
 #endif /*LCD_C_*/
